0d23fa41d477a77e198467e3bfe33e01418ef06b
ju-barbosa <jubarbosa2211@gmail.com>
2026-03-22 20:04:59 +0000

    figurinhas

--- ed/baruel/go/draft.go
+++ ed/baruel/go/draft.go
@@ -1,40 +1,41 @@
 package main
 import "fmt"
 func main() {
-  var N, total int
-  fmt.Scan(&N, &total)
-
-  vetor := make([]int, total)
-
-  repetidas := -1
-  faltam := -1
   
+  var qtd_album, qtd_possui int
+  fmt.Scan(&qtd_album, &qtd_possui)
 
-  for i := 0; i < total; i++ {
-	fmt.Scan(&vetor[i])
+  figurinhas := make([]int, qtd_possui)
+  for i := range figurinhas{
+    fmt.Scan(&figurinhas[i])
   }
 
-   var repete [100] int 
+  unicos := make(map[int]bool)
+  repetidos := make([]int, 0, qtd_possui)
 
-	for i := 0; i < total; i++ {
-		for j := 0; j < total; j++{
-			if vetor[i] == vetor[j] {
-				repete[i] = 
-			}
-		}
-		
-	}
-
-
-  if repetidas == - 1 {
-	fmt.Scan("N\n")
-  } else {
-	fmt.Printf("%v\n", repetidas)
+  for _, figura := range figurinhas {
+    if unicos[figura]{
+      repetidos = append(repetidos, figura)
+    } else {
+      unicos[figura] = true
+    }
   }
-
-  if faltam == -1 {
-	fmt.Scan("S\n")
+  saida := fmt.Sprintf("%v", repetidos)
+  if saida == "[]"{
+    fmt.Println("N")
+  }else{
+    fmt.Println(saida[1 : len(saida)-1])
+  }
+  faltantes := make([]int, 0, qtd_album)
+  for i := 1; i <= qtd_album; i++{
+    if !unicos[i]{
+      faltantes = append(faltantes, i)
+    }
+  }
+  saida_f := fmt.Sprintf("%v", faltantes)
+  if saida_f == "[]"{
+    fmt.Println("N")
   } else {
-	fmt.Printf("%v\n", faltam)
+    fmt.Println(saida_f[1 : len(saida_f)-1])
   }
 }
