5032b8751ffd96f156f3e112ca5ceac0efd03a4e
ju-barbosa <jubarbosa2211@gmail.com>
2026-03-28 23:39:53 +0000

    princesa

--- ed/apagando/go/draft.go
+++ ed/apagando/go/draft.go
@@ -1,5 +1,15 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("qxcode")
+    var fila_inicial, fila_sairam int
+    fmt.Scan(&fila_inicial)
+    
+
+    inicial := make([]int, fila_inicial)
+
+    fmt.Scan(&fila_sairam)
+
+    sairam := make([]int, fila_sairam)
+
+
 }
--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -5,6 +5,22 @@ func main() {
     fmt.Scan(&n, &e)
     
     fila := make([]int, 1, n)
-    mortos := make(map[int]int)
+    mortos := make(map[int]bool)
+    
+
+    for i := range fila {
+        fmt.Scan(&fila[i])
+    }
+
+    for _, pessoa := range fila {
+        if mortos[pessoa] {
+           
+        } else {
+            mortos[pessoa] = true
+            
+        }
+        }
+    }
+    
 
 }
