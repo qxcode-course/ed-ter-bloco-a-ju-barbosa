5b1a9885625143df7cfec91014a25a30ac39a46b
ju-barbosa <jubarbosa2211@gmail.com>
2026-03-29 01:18:18 +0000

    apagando

--- ed/apagando/go/draft.go
+++ ed/apagando/go/draft.go
@@ -2,14 +2,35 @@ package main
 import "fmt"
 func main() {
     var fila_inicial, fila_sairam int
-    fmt.Scan(&fila_inicial)
-    
 
-    inicial := make([]int, fila_inicial)
+    fmt.Scan(&fila_inicial)
+    id_inicial := make([]int, fila_inicial) 
+    for i := 0; i < fila_inicial; i++ {
+        fmt.Scan(&id_inicial[i])
+    }
 
     fmt.Scan(&fila_sairam)
+    id_sairam := make([]int, fila_sairam)
+    for i := range id_sairam {
+        fmt.Scan(&id_sairam[i])
+    }
 
-    sairam := make([]int, fila_sairam)
+    saiu := make(map[int]bool)
 
+    for _, sairam := range id_sairam {
+          saiu[sairam] = true
+        
+    }
 
+    fila_final := make([]int, 0)
+
+    for _, sairam := range id_inicial {
+        if !saiu[sairam] {
+            fila_final = append(fila_final, sairam)
+        }
+    }
+
+    saida := fmt.Sprintf("%v", fila_final)
+    fmt.Printf(saida[1 : len(saida)-1])
+    fmt.Println(" ")
 }
