ba5bbe2ec802ad97a3f72f50aac1517d981d6dfd
ju-barbosa <jubarbosa2211@gmail.com>
2026-04-03 03:07:40 +0000

    divisao

--- ed/resto/go/draft.go
+++ ed/resto/go/draft.go
@@ -1,5 +1,19 @@
 package main
 import "fmt"
-func main() {
-    fmt.Println("Hello, World!")
-}
+
+    func dividir(num int) {
+        if (num == 0) {
+            return
+        }
+     divisao := num/2
+     resto := num % 2
+    dividir(divisao)
+    fmt.Printf("%v %v\n", divisao, resto)
+    }
+
+    func main(){
+        var num int
+        fmt.Scan(&num)
+        (dividir(num))
+    }
+
