8f35a37d750bdb2524558ac80f502366ae577413
ju-barbosa <jubarbosa2211@gmail.com>
2026-04-06 19:53:27 +0000

    vetequalsa

---
+++ ed/vetequals/src/go/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func EhVazio(vet []int) bool {
+	return fmt.Sprint(vet) == "[]"
+}
+
+// não altere a assinatura
+func equals(a []int, b []int) bool {
+	// não use a função len para ver ou comparar os tamanhos
+	if EhVazio(a) && EhVazio(b) { // utilize a função EhVazio para ver se o vetor é vazio
+		return true
+	}
+	if EhVazio(a) || EhVazio(b){
+		return false
+	}
+ 	if a[0] != b[0]{ // você só pode consultar o primeiro elemento do vetor
+		return false
+	}
+	// e não pode usar nenhum tipo de laço
+	return equals(a[1:], b[1:]) // Use recursao para consultar os outros elementos equals(a[1:], b[1:])
+	// não altere o protótipo da função nem crie funções auxiliares
+	_, _ = a, b
+	return false
+
+	
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+	vet1 := str2slice(scanner.Text())
+	scanner.Scan()
+	vet2 := str2slice(scanner.Text())
+	if equals(vet1, vet2) {
+		fmt.Println("iguais")
+	} else {
+		fmt.Println("diferentes")
+	}
+}
+
+func str2slice(line string) []int {
+	parts := strings.Fields(line)
+	nums := make([]int, 0)
+	for i := 1; i < len(parts)-1; i++ {
+		value, _ := strconv.Atoi(parts[i])
+		nums = append(nums, value)
+	}
+	return nums
+}
