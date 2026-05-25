c9a228278a2ce92181079a728c23c11d1eef8b81
ju-barbosa <jubarbosa2211@gmail.com>
2026-05-25 00:35:39 +0000

    princesa

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -1,34 +1,46 @@
-jpackage main
+package main
 import "fmt"
 
-func print_fila(fila[]int, espada int) {
-    fmt.print(" ")
-
-    for i, elemento
+func print_jogadores(jogadores[]int, espada int) {
+    fmt.Print("[ ")
+    for i, elemento := range jogadores{
+        if elemento == 0{
+            continue
+        }
+        fmt.Print(elemento)
+        if i == espada{
+            fmt.Print(">")
+        }
+        fmt.Print(" ")
+    }
+    fmt.Print("]\n")
 }
 
-func procurar_vivo(){
+func procurar_vivo(jogadores[] int, espada int) int{
     for {
-        if fila[espada] = len(fila)
-        if fila[espada != 0 
+       espada = (espada + 1) % len(jogadores)
+       if jogadores[espada] != 0{
         return espada
+       }
         }
     }
-}
+    
 func main() {
     var qtd, espada int
     fmt.Scan(&qtd, &espada)
-    
 
-    
-for i := 1; i <= qtd; i++{
-    fila = append(fila, i)
+    jogadores:= make([]int, 0, qtd)
+
+    for i := 1; i <= qtd; i++{
+    jogadores= append(jogadores, i)
 }
-
    espada -= 1
     for range qtd -1{
-        print_fila := procurar_vivo(fila, espada)
+        print_jogadores(jogadores, espada)
+        vai_morrer := procurar_vivo(jogadores, espada)
+        jogadores[vai_morrer] = 0
+        espada = procurar_vivo(jogadores, espada)
         
     }
-
+    print_jogadores(jogadores, espada)
 }
