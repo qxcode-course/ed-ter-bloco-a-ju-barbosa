6e5c878606550ace0a8f7cb12fb1316b2c6963b4
ju-barbosa <jubarbosa2211@gmail.com>
2026-06-28 20:06:07 +0000

    princesa 2

--- ed/alternando/src/go/draft.go
+++ ed/alternando/src/go/draft.go
@@ -9,53 +9,58 @@ func print_jogadores(jogadores[]int, espada int) {
         if elemento == 0{
             continue
         }
-        
-        if i == espada && elemento > 0{
-            fmt.Print("%d>", elemento)
+        if i == espada {
+            if elemento > 0 {
+                 fmt.Printf("%d>", elemento)
+            } else {
+                  fmt.Printf("<%d", elemento)
+            }
+        } else {
+            fmt.Printf("%d", elemento)
         }
-        
-        if i == espada && elemento < 0{
-            fmt.Print("%d<", elemento)
+            fmt.Print(" ")
         }
-        fmt.Print(elemento)
-
-    fmt.Print("")
-    }
-
-    fmt.Print("]\n")
+    fmt.Print("]\n")        
 }
 
-func procurar_vivo(jogadores[] int, espada int) int{
+
+func procurar_vivo(jogadores[] int, pos int, direcao int) int{
     n := len(jogadores)
     for {
-       espada = (espada + 1) % n
-       if espada < 0 {
-        espada += n
+       pos = (pos + direcao + n) % n
+       if jogadores[pos] != 0 {
+        return pos
        }
-
-       if jogadores[espada] != 0{
-        return espada
-       }
-
-        }
     }
+}
+
 
 func main() {
-    var qtd, espada int
-    fmt.Scan(&qtd, &espada)
+    var qtd, espada, fase int
+    fmt.Scan(&qtd, &espada, &fase)
 
     jogadores:= make([]int, 0, qtd)
+    sinal := fase 
 
     for i := 1; i <= qtd; i++{
-    jogadores= append(jogadores, i)
+    jogadores= append(jogadores, i*sinal)
+    sinal *= -1
 }
    espada -= 1
-    for range qtd -1{
+   vivos := qtd
+
+    for vivos > 1{
         print_jogadores(jogadores, espada)
-        vai_morrer := procurar_vivo(jogadores, espada)
-        jogadores[vai_morrer] = 0
-        espada = procurar_vivo(jogadores, espada)
-        
+
+        direcao := 1   
+        if jogadores[espada] < 0 {
+            direcao = -1
+        } 
+        alvo := procurar_vivo(jogadores, espada, direcao)
+        jogadores[alvo] = 0
+        vivos--
+        espada = procurar_vivo(jogadores, espada, direcao)
+
     }
     print_jogadores(jogadores, espada)
 }
